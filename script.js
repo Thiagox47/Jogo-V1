8bdb37df3b90abbbb7fed8f523df96a4cc6e3293
THIAGO VINICIUS <othiago.vss.20@gmail.com>
2022-12-23 14:19:13 -0300

    Adicionados Picolé e feitas algumas alterações
    Falta ser configurada a soma dos valores recebidos ao vender picolé.

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ alertPage.style.display = "none";
 let juices = 1;
 let juiceMultiplys = 1;
 let valueMultiplyJuices = 10;
-let valueSellerJuices = 350;
+let valueSellerJuices = 1500;
 let timerJuiceClick = 3;
-let juiceInterval;
+let popsicles = 3.5;
+let popsicleMultiplys = 1;
+let valueMuliplyPopsicles = 35;
 //Dom de todos os botões de geradores invisiveis e variaveis
 let juice_of_orange = document.getElementById("juice_of_orange")
-document.getElementById("perfume_of_magazine").style.display = 'none';
+document.getElementById("popsicle").style.display = 'none';
 document.getElementById("computer_maintenance").style.display = 'none';
 document.getElementById("drop_Shipping").style.display = 'none';
 document.getElementById("Bitcoin").style.display = 'none';
@@ -23,7 +25,7 @@ document.getElementById("loan_sharking").style.display = 'none';
 //Dom de todos os botões de Upgrade invisiveis no html e variaveis
 document.getElementById("juiceMult").style.display = 'none';
 let valueJuiceMult = document.getElementById("juiceMult");
-document.getElementById("sellerJui").style.display = 'none';
+let valuePopsicles = document.getElementById("moreFlavors");
 document.getElementById("technician").style.display = 'none';
 document.getElementById("website").style.display = 'none';
 document.getElementById("queryBit").style.display = 'none';
@@ -36,16 +38,17 @@ function buttomEnable() {
     if (valueMultiplyJuices <= result) {
         document.getElementById("juiceMult").style.display = 'block';
     }
-    if (valueSellerJuices != 'none') {
-        if (result >= valueSellerJuices) {
-            document.getElementById("sellerJui").style.display = 'block';
-        } else {
-            document.getElementById("sellerJui").style.display = 'none';
-        }
+    if (valueSellerJuices <= result) {
+        document.getElementById("sellerJui").style.display = 'block';
+    } else {
+        document.getElementById("sellerJui").style.display = 'none';
     }
-
 }
-// Função para o tempo do alert div
+if (valueMuliplyPopsicles < result) {
+    document.getElementById("moreFlavors").style.display = 'block';
+}
+buttomEnable()
+    // Função para o tempo do alert div
 function mesage_div_Clock() {
     let clock_mesage = setTimeout(() => {
         alertPage.style.display = 'none';
@@ -53,14 +56,29 @@ function mesage_div_Clock() {
     }, 3000);
 }
 
+//Função de quando o botão de gerador e utilizado.
+function juice() {
+    //valor de juice sempre 1, porém podendo ser multiplicado.
+    juices = 1;
+    juices = aplyMultiply(juices, juiceMultiplys)
+    resultOperations(juices, '+');
+    //valor concatenado no html
+    showMoney()
+        //Mostrar valor de juices 
+    console.log(juices);
+    buttomEnable()
+    juice_of_orange.innerHTML = `Suco de laranja $ ${juices.toFixed(2)}`
+
+}
+
 //Função para receber valor de juice ao comprar e multiplicador com balanceamento de multiplicação.
 function juiceMultiply() {
     if (result >= valueMultiplyJuices) {
-        juiceMultiplys += 0.5;
+        juiceMultiplys += 1.5;
         resultOperations(valueMultiplyJuices, "-")
         valueMultiplyJuices = multiplyGenerationUpgrade(valueMultiplyJuices)
         showMoney()
-        valueJuiceMult.innerHTML = `Laranjão -$ ${valueMultiplyJuices.toFixed(2)}`;
+        valueJuiceMult.innerHTML = `Caixa de laranja -$ ${valueMultiplyJuices.toFixed(2)}`;
         //aparece a informação do upgrade e adiciona um timer
         alertPage.innerHTML = "Upgrade de multiplicação habilitado!"
         alertPage.style.display = 'block';
@@ -75,19 +93,42 @@ function juiceMultiply() {
     console.log(juiceMultiplys);
 }
 
-//Função de quando o botão de gerador e utilizado.
-function juice() {
-    //valor de juice sempre 1, porém podendo ser multiplicado.
-    juices = 1;
-    juices = aplyMultiply(juices, juiceMultiplys)
-    resultOperations(juices, '+');
-    //valor concatenado no html
-    showMoney()
-        //Mostrar valor de juices 
-    console.log(juices);
-    buttomEnable()
-    juice_of_orange.innerHTML = `Suco de laranja +$ ${juices.toFixed(2)}`
+//Clique automatico
+function sellerJuice() {
+    if (result >= valueSellerJuices) {
+        resultOperations(valueSellerJuices, "-");
+        document.getElementById("sellerJui").style.display = 'none';
+        let juiceInterval = setInterval(juice, timerJuiceClick * 1000);
+        valueSellerJuices = multiplyGenerationUpgrade(valueSellerJuices)
+        alertPage.innerHTML = "Vendedor de sucos habilitado!"
+        alertPage.style.display = 'block';
+        mesage_div_Clock()
+    } else {
+        alert("Você não tem saldo suficiente para isso!")
+    }
+    console.log(valueSellerJuices)
+}
 
+function more_flavors() {
+    document.getElementById("popsicle").style.display = 'block';
+    if (result >= valueMuliplyPopsicles) {
+        popsicleMultiplys += 3;
+        resultOperations(valueMuliplyPopsicles, "-")
+        valueMultiplyJuices = multiplyGenerationUpgrade(valueMuliplyPopsicles)
+        showMoney()
+        valuePopsicles.innerHTML = `Mais sabores de Picolé -$ ${valueMuliplyPopsicles.toFixed(2)}`;
+        //aparece a informação do upgrade e adiciona um timer
+        alertPage.innerHTML = "Upgrade de multiplicação habilitado!"
+        alertPage.style.display = 'block';
+        mesage_div_Clock()
+    } else {
+        //aparece a informação de dinheiro insuficiente e adiciona um timer
+        alertPage.style.display = 'block';
+        alertPage.innerHTML = "Você não tem saldo suficiente para isso!"
+        mesage_div_Clock()
+    }
+    //mostrar no console o valor do multiplicador.
+    console.log(popsicleMultiplys);
 }
 
 //Função para facilitar criação de multiplicadores e geradores
@@ -116,19 +157,4 @@ function multiplyGenerationUpgrade(generationUpgradeValues) {
 //Aplicar a multiplicação no gerador
 function aplyMultiply(generation, multiplye) {
     return generation *= multiplye;
-}
-//Clique automatico
-function sellerJuice() {
-    if (result >= valueSellerJuices) {
-        resultOperations(valueSellerJuices, "-");
-        document.getElementById("sellerJui").style.display = 'none';
-        juiceInterval = setInterval(juice, timerJuiceClick * 1000);
-        valueSellerJuices = multiplyGenerationUpgrade(valueSellerJuices)
-        alertPage.innerHTML = "Vendedor de sucos habilitado!"
-        alertPage.style.display = 'block';
-        mesage_div_Clock()
-    } else {
-        alert("Você não tem saldo suficiente para isso!")
-    }
-    console.log(valueSellerJuices)
 }
